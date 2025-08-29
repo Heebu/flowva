fc8532ec50af836486e0da23458e011fb523a066
Idris Adedeji <106799410+Heebu@users.noreply.github.com>
2025-08-29 09:53:28 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: ScreenUtilInit(
+        designSize: const Size(450.0, 700.0),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (BuildContext context, Widget? child) {
+          ScreenUtil.init(context);
+          return MaterialApp(
+            title: 'Flowva',
+            debugShowCheckedModeBanner: false,
+            home: Scaffold(),
+          );
+        },
+      ),
+    );
+  }
+}
